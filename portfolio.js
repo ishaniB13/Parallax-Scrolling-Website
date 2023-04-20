43f7bd349b6f2653bffb81771cada4c5b866d0f7
Ishani Banerjee <ishani.banerjee13@gmail.com>
2023-04-20 15:24:25 +0530

    Portfolio

---
+++ portfolio.js
@@ -0,0 +1,188 @@
+gsap.registerPlugin(ScrollTrigger);
+gsap.to("#object-1", {
+    scrollTrigger: {
+    scrub: 5,
+    markers: false
+    },
+    scale: 2.5,
+    xPercent: -150
+    })
+    gsap.to("#object-2", {
+        scrollTrigger: {
+        scrub: 5,
+        markers: false
+        },
+        scale: 1.5,
+        xPercent: 150
+        })
+        gsap.to("#object-3", {
+            scrollTrigger: {
+            scrub: 5,
+            markers: false
+            },
+            scale: 1.8,
+            yPercent: 250
+            })
+            gsap.to("#caption1", {
+                scrollTrigger: {
+                scrub: 5,
+                markers: false
+                },
+                opacity: 1,
+                scale: 0.5,
+                yPercent: 370,
+                xPercent: -60
+                })
+            gsap.to("#object-4", {
+                scrollTrigger: {
+                scrub: 5,
+                markers: false
+                },
+                opacity: 0.8,
+                scale: 2,
+                yPercent: -300,
+                xPercent: -300
+                })
+                gsap.to("#caption2", {
+                    scrollTrigger: {
+                    scrub: 5,
+                    markers: false
+                    },
+                    opacity: 1,
+                    scale: 1,
+                    yPercent: -800,
+                    xPercent: -300
+                    })
+    
+                    
+                        gsap.timeline()
+.to("#caption1", { opacity: 0, duration: 1, ease: "linear" })
+.to("#caption1", { opacity: 0, duration: 2, ease: "linear" });
+gsap.to("#object-5", {
+    scrollTrigger: {
+    scrub: 5,
+    markers: false
+    },
+    opacity: 1,
+    duration: 0.75,
+    scale: 1.5,
+    yPercent: 300,
+    xPercent: -30
+    })
+    gsap.to("#caption3", {
+        scrollTrigger: {
+        scrub: 5,
+        markers: false
+        },
+        opacity: 1,
+        scale: 1,
+        yPercent: 250,
+        xPercent: -30
+        })
+    gsap.to("#object-6", {
+        scrollTrigger: {
+        scrub: 5,
+        markers: false
+        },
+        opacity: 1,
+        scale: 1.5,
+        yPercent: -200,
+        xPercent: -30
+        })
+        gsap.to("#caption4", {
+            scrollTrigger: {
+            scrub: 5,
+            markers: false
+            },
+            opacity: 0.8,
+            scale: 1.5,
+            yPercent: -500,
+            xPercent: -10
+            })
+        gsap.to("#object-7", {
+            scrollTrigger: {
+            scrub: 10,
+            markers: false
+            },
+            opacity: 1,
+            scale: 1.5,
+            yPercent: -10,
+            xPercent: 200
+            })
+            gsap.to("#caption5", {
+                scrollTrigger: {
+                scrub: 10,
+                markers: false
+                },
+                opacity: 1,
+                scale: 1.5,
+                yPercent: -100,
+                xPercent: 180
+                })
+                gsap.to("#object-8", {
+                    scrollTrigger: {
+                    scrub: 5,
+                    markers: false
+                    },
+                    opacity: 1,
+                    scale: 1.5,
+                    yPercent: -10,
+                    xPercent: -520
+                    })
+                    gsap.to("#caption6", {
+                        scrollTrigger: {
+                        scrub: 5,
+                        markers: false
+                        },
+                        opacity: 1,
+                        scale: 1.5,
+                        yPercent: 20,
+                        xPercent: -470
+                        })
+                        gsap.to("#object-9", {
+                            scrollTrigger: {
+                            scrub: 5,
+                            markers: false
+                            },
+                            opacity: 1,
+                            scale: 1.5,
+                            yPercent: 50,
+                            xPercent: 160
+                            })
+                                gsap.to("#caption7", {
+                                    scrollTrigger: {
+                                    scrub: 5,
+                                    markers: false
+                                    },
+                                    opacity: 1,
+                                    scale: 1.5,
+                                    yPercent: -70,
+                                    xPercent: -300,
+                                    })
+                                    gsap.to("#object-11", {
+                                        scrollTrigger: {
+                                        scrub: 5,
+                                        markers: false
+                                        },
+                                        opacity: 1,
+                                        scale: 6,
+                                        yPercent: 20,
+                                        xPercent: 250
+                                        })
+                                        gsap.to("#contact", {
+                                            scrollTrigger: {
+                                            scrub: 5,
+                                            markers: false
+                                            },
+                                            opacity: 1,
+                                            scale: 1.3,
+                                            yPercent: 100,
+                                            xPercent: 230
+                                            })
+                                       
+                                    
+
+                
+                                                
+
+                  
